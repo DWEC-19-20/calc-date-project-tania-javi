bf2b51dd3da72295de917b638835a01ac1a975f6
javimoya11 <javimoya11@gmail.com>
2019-11-04 08:28:04 +0100

    Programa acabado y testeado

--- test/js/test.workingDays.js
+++ test/js/test.workingDays.js
@@ -3,18 +3,30 @@ var assert = chai.assert,
 
 suite("Testing calcDate", function() {
     test("Test pasado", function() {
-        expect(calcWorkingDate(new Date(2019, 10, 13),14)).to.equal("31/10/2019");
+        expect(calcWorkingDate(new Date("2019-11-15"),15)).to.equal("9/12/2019");
     });
     test("Test pasado", function() {
-        expect(calcWorkingDate(new Date(2019, 10, 31),-14)).to.equal("13/10/2019");
+        expect(calcWorkingDate(new Date("2019-11-15"),-15)).to.equal("24/10/2019");
+    });
+    test("Test pasado", function() {
+        expect(calcWorkingDate(new Date("2019-11-15"),300)).to.equal("22/1/2021");
+    });
+    test("Test pasado", function() {
+        expect(calcWorkingDate(new Date("2019-11-15"),-300)).to.equal("11/9/2018");
     });
 });
 
 suite("Testing getDays", function() {
     test("Test pasado", function() {
-        expect(getWorkingDays(new Date(2019, 10, 13),new Date(2019, 10, 31))).to.equal(14);
+        expect(getWorkingDays(new Date("2019-11-15"),new Date("2019-12-09"))).to.equal(15);
     });
     test("Test pasado", function() {
-        expect(getWorkingDays(new Date(2019, 28, 31),new Date(2019, 10, 13))).to.equal(-14);
+        expect(getWorkingDays(new Date("2019-11-15"),new Date("2019-10-24"))).to.equal(-15);
+    });
+    test("Test pasado", function() {
+        expect(getWorkingDays(new Date("2019-11-15"),new Date("2021-01-22"))).to.equal(300);
+    });
+    test("Test pasado", function() {
+        expect(getWorkingDays(new Date("2019-11-15"),new Date("2018-09-11"))).to.equal(-300);
     });
 });
