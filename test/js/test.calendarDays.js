61f5d5575560108cf627437b67d8c5ab6dd93bc2
unknown <tarumendentany@gmail.com>
2019-11-04 09:30:11 +0100

    Programa acabado y testeado

--- test/js/test.calendarDays.js
+++ test/js/test.calendarDays.js
@@ -11,6 +11,18 @@ suite("Testing calcDate", function() {
     test("Test pasado", function() {
         expect(calcDate(new Date("2019-10-10"),10953)).to.equal("5/10/2049");
     });
+    test("Test pasado", function() {
+        expect(calcDate(new Date("2019-11-03"),396)).to.equal("3/12/2020");
+    });
+    test("Test pasado", function() {
+        expect(calcDate(new Date("1970-01-01"),9612)).to.equal("26/4/1996");
+    });
+    test("Test pasado", function() {
+        expect(calcDate(new Date("742-05-01"),26193)).to.equal("16/1/814");
+    });
+    test("Test pasado", function() {
+        expect(calcDate(new Date("1776-07-04"),792)).to.equal("3/9/1778");
+    });
 });
 
 suite("Testing getDays", function() {
@@ -22,6 +34,18 @@ suite("Testing getDays", function() {
     });
     test("Test pasado", function() {
         expect(getDays(new Date(2019, 09, 10),new Date(2049, 09, 05))).to.equal(10953);
+    });
+    test("Test pasado", function() {
+        expect(getDays(new Date(2019, 10, 03),new Date(2020, 11, 03))).to.equal(396);
+    });
+    test("Test pasado", function() {
+        expect(getDays(new Date(1970, 00, 01),new Date(1996, 03, 26))).to.equal(9612);
+    });
+    test("Test pasado", function() {
+        expect(getDays(new Date(742, 04, 01),new Date(814, 00, 16))).to.equal(26193);
+    });
+    test("Test pasado", function() {
+        expect(getDays(new Date(1776, 06, 04),new Date(1778, 08, 03))).to.equal(791);
     });
 });
 
--- test/js/test.workingDays.js
+++ test/js/test.workingDays.js
@@ -14,6 +14,12 @@ suite("Testing calcDate", function() {
     test("Test pasado", function() {
         expect(calcWorkingDate(new Date("2019-11-15"),-300)).to.equal("11/9/2018");
     });
+    test("Test pasado", function() {
+        expect(calcWorkingDate(new Date("2019-09-01"),118)).to.equal("19/2/2020");
+    });
+    test("Test pasado", function() {
+        expect(calcWorkingDate(new Date("1989-03-07"),-261)).to.equal("26/2/1988");
+    });
 });
 
 suite("Testing getDays", function() {
@@ -28,5 +34,11 @@ suite("Testing getDays", function() {
     });
     test("Test pasado", function() {
         expect(getWorkingDays(new Date("2019-11-15"),new Date("2018-09-11"))).to.equal(-300);
+    });
+    test("Test pasado", function() {
+        expect(getWorkingDays(new Date("2019-09-01"),new Date("2020-02-19"))).to.equal(118);
+    });
+    test("Test pasado", function() {
+        expect(getWorkingDays(new Date("1989-03-07"),new Date("1988-02-26"))).to.equal(-261);
     });
 });
